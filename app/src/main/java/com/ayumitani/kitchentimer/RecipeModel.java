f62fad13e8f7bab44f6400eee7ea5f2ca2c1168e
ayumitani <ayumi.minamitani@gmail.com>
2018-05-26 16:34:09 +0900

    レシピの編集・更新機能、削除機能、新規idの作成機能を実装

--- app/src/main/java/com/ayumitani/kitchentimer/RecipeModel.java
+++ app/src/main/java/com/ayumitani/kitchentimer/RecipeModel.java
@@ -1,30 +1,72 @@
 package com.ayumitani.kitchentimer;
 
+import javax.annotation.Nullable;
+
 import io.realm.Realm;
 
 public class RecipeModel {
 
     private Realm mRealm = Realm.getDefaultInstance();
 
-    public Recipe createRecipe(String recipeName, String cookingTime) {
-        mRealm.beginTransaction();
-        Recipe recipe = mRealm.createObject(Recipe.class, makeRecipeId());
+    /**
+     * レシピの新規作成もしくは編集の更新
+     *
+     * @param recipeId    　レシピのid
+     * @param recipeName  　レシピの名前
+     * @param cookingTime 　調理時間
+     */
+    public void copyOrUpdate(@Nullable Integer recipeId, String recipeName, String cookingTime) {
+        if (recipeId == null) {
+            recipeId = makeRecipeId();
+        }
+        Recipe recipe = new Recipe();
+        recipe.setId(recipeId);
         recipe.setRecipe(recipeName);
         recipe.setTime(cookingTime);
+
+        mRealm.beginTransaction();
+
+        mRealm.copyToRealmOrUpdate(recipe);
+
         mRealm.commitTransaction();
-        return recipe;
     }
 
-    private int makeRecipeId(){
+
+    /**
+     * レシピの削除
+     *
+     * @param recipeId 　レシピのid
+     */
+    public void deleteRecipe(int recipeId) {
+        mRealm.beginTransaction();
+
+        // レシピの一覧から、recipeIdと一致するidのレシピの中で最初のものを取り出す
+        Recipe recipe = mRealm.where(Recipe.class).equalTo("id", recipeId).findFirst();
+        if (recipe != null) {
+            recipe.deleteFromRealm();
+        }
+
+        mRealm.commitTransaction();
+    }
+
+
+    /**
+     * レシピのidを作成
+     *
+     * @return レシピのid
+     */
+    private int makeRecipeId() {
         int recipeId = 1;
 
+        //レシピの一覧から、idの最大値を取り出す
         Number maxRecipeId = mRealm.where(Recipe.class).max("id");
 
-        if (maxRecipeId != null){
+        if (maxRecipeId != null) {
             recipeId = maxRecipeId.intValue() + 1;
         }
 
         return recipeId;
     }
+
 
 }
