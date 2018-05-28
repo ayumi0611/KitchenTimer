8f9e5babb5a1c076f8d37fe02ce029dde10338b8
ayumitani <ayumi.minamitani@gmail.com>
2018-05-28 21:43:50 +0900

    FloatingActionButtonの実装

--- app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
+++ app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
@@ -3,11 +3,17 @@ package com.ayumitani.kitchentimer;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
 
 public class HomeFragment extends Fragment{
+
+    Fragment mFragment = this;
 
     @Nullable
     @Override
@@ -15,5 +21,27 @@ public class HomeFragment extends Fragment{
         super.onCreateView(inflater, container, savedInstanceState);
 
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //viewをidから取得する
+        FloatingActionButton transitionRecipeListButton = view.findViewById(R.id.transition_recipe_list);
+        // viewにOnClickListenerをセットする
+        transitionRecipeListButton.setOnClickListener(new OnClickTransitionRecipeListListener());
+
+    }
+
+    // OnClickListenerを内包してる
+    private class OnClickTransitionRecipeListListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            // 実際の処理を記述する
+            getActivity().getFragmentManager().beginTransaction()
+                    .hide(HomeFragment.this)
+                    .add(R.id.fragment_container, new RecipeListFragment()).commit();
+        }
     }
 }
