971999fb048bb4c1c12925008278d279f5b5d5d8
ayumitani <ayumi.minamitani@gmail.com>
2018-05-29 01:17:08 +0900

    button_plus、button_minusのviewの取得 viewにOnClickListenerのセット

--- app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
+++ app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
@@ -8,10 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.QuickContactBadge;
 import android.widget.Toast;
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
 
     Fragment mFragment = this;
 
@@ -27,15 +28,39 @@ public class HomeFragment extends Fragment{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //viewをidから取得する
+        // viewをidから取得する
         FloatingActionButton transitionRecipeListButton = view.findViewById(R.id.transition_recipe_list);
         // viewにOnClickListenerをセットする
         transitionRecipeListButton.setOnClickListener(new OnClickTransitionRecipeListListener());
 
+        // button_plusのviewをidから取得する
+        ImageButton button_plus_1 = view.findViewById(R.id.button_plus_1);
+        ImageButton button_plus_2 = view.findViewById(R.id.button_plus_2);
+        ImageButton button_plus_3 = view.findViewById(R.id.button_plus_3);
+        ImageButton button_plus_4 = view.findViewById(R.id.button_plus_4);
+
+        // button_plusのviewにOnClickListenerをセットする
+        button_plus_1.setOnClickListener(new OnClickButtonPlus1());
+        button_plus_2.setOnClickListener(new OnClickButtonPlus2());
+        button_plus_3.setOnClickListener(new OnClickButtonPlus3());
+        button_plus_4.setOnClickListener(new OnClickButtonPlus4());
+
+        // button_minusのviewをidから取得する
+        ImageButton button_minus_1 = view.findViewById(R.id.button_minus_1);
+        ImageButton button_minus_2 = view.findViewById(R.id.button_minus_2);
+        ImageButton button_minus_3 = view.findViewById(R.id.button_minus_3);
+        ImageButton button_minus_4 = view.findViewById(R.id.button_minus_4);
+
+        //　button_minusのviewにOnClickListenerをセットする
+        button_minus_1.setOnClickListener(new OnClickButtonMinus1());
+        button_minus_2.setOnClickListener(new OnClickButtonMinus2());
+        button_minus_3.setOnClickListener(new OnClickButtonMinus3());
+        button_minus_4.setOnClickListener(new OnClickButtonMinus4());
+
     }
 
     // OnClickListenerを内包してる
-    private class OnClickTransitionRecipeListListener implements View.OnClickListener{
+    private class OnClickTransitionRecipeListListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
             // 実際の処理を記述する
@@ -44,4 +69,5 @@ public class HomeFragment extends Fragment{
                     .add(R.id.fragment_container, new RecipeListFragment()).commit();
         }
     }
+
 }
