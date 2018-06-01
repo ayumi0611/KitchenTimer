f689a38e5dbba2e28c8d6820c7ae4ea31b31cf80
ayumitani <ayumi.minamitani@gmail.com>
2018-06-02 00:52:58 +0900

    OnClickButtonの実装

--- app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
+++ app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.QuickContactBadge;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
@@ -74,29 +75,67 @@ public class HomeFragment extends Fragment {
     private class OnClickButtonPlus1 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.ten_minutes);
+            String replaceTenMinutes = textView.getText().toString();
+            int tenMinutes = Integer.parseInt(replaceTenMinutes);
+            if (tenMinutes >= 9){
+                tenMinutes = 0;
+            } else {
+                tenMinutes++;
+            }
+            replaceTenMinutes = Integer.toString(tenMinutes);
+            textView.setText(replaceTenMinutes);
+            }
 
         }
-    }
 
     private class OnClickButtonPlus2 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.one_minutes);
+            String replaceOneMinutes= textView.getText().toString();
+            int oneMinutes = Integer.parseInt(replaceOneMinutes);
+            if (oneMinutes >= 9){
+                oneMinutes = 0;
+            } else {
+                oneMinutes++;
+            }
+            replaceOneMinutes = Integer.toString(oneMinutes);
+            textView.setText(replaceOneMinutes);
         }
+
     }
 
     private class OnClickButtonPlus3 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.ten_seconds);
+            String replaceTenSeconds = textView.getText().toString();
+            int tenSeconds = Integer.parseInt(replaceTenSeconds);
+            if (tenSeconds >= 5){
+                tenSeconds = 0;
+            } else {
+                tenSeconds++;
+            }
+            replaceTenSeconds = Integer.toString(tenSeconds);
+            textView.setText(replaceTenSeconds);
         }
+
     }
 
     private class OnClickButtonPlus4 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.one_seconds);
+            String replaceOneSeconds = textView.getText().toString();
+            int oneSeconds = Integer.parseInt(replaceOneSeconds);
+            if (oneSeconds >= 9){
+                oneSeconds = 0;
+            } else {
+                oneSeconds++;
+            }
+            replaceOneSeconds = Integer.toString(oneSeconds);
+            textView.setText(replaceOneSeconds);
         }
     }
 
@@ -104,28 +143,64 @@ public class HomeFragment extends Fragment {
     private class OnClickButtonMinus1 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.ten_minutes);
+            String replaceTenMinutes = textView.getText().toString();
+            int tenMinutes = Integer.parseInt(replaceTenMinutes);
+            if (tenMinutes <= 0){
+                tenMinutes = 9;
+            } else {
+                tenMinutes--;
+            }
+            replaceTenMinutes = Integer.toString(tenMinutes);
+            textView.setText(replaceTenMinutes);
         }
     }
 
     private class OnClickButtonMinus2 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.one_minutes);
+            String replaceOneMinutes = textView.getText().toString();
+            int oneMinutes = Integer.parseInt(replaceOneMinutes);
+            if (oneMinutes <= 0){
+                oneMinutes = 9;
+            } else {
+                oneMinutes--;
+            }
+            replaceOneMinutes = Integer.toString(oneMinutes);
+            textView.setText(replaceOneMinutes);
         }
     }
 
     private class OnClickButtonMinus3 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.ten_seconds);
+            String replaceTenSeconds = textView.getText().toString();
+            int tenSeconds = Integer.parseInt(replaceTenSeconds);
+            if (tenSeconds <= 0){
+                tenSeconds = 5;
+            } else {
+                tenSeconds--;
+            }
+            replaceTenSeconds = Integer.toString(tenSeconds);
+            textView.setText(replaceTenSeconds);
         }
     }
 
     private class OnClickButtonMinus4 implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-
+            TextView textView = getView().findViewById(R.id.one_seconds);
+            String replaceOneSeconds = textView.getText().toString();
+            int oneSeconds = Integer.parseInt(replaceOneSeconds);
+            if (oneSeconds <= 0){
+                oneSeconds = 9;
+            } else {
+                oneSeconds--;
+            }
+            replaceOneSeconds = Integer.toString(oneSeconds);
+            textView.setText(replaceOneSeconds);
         }
     }
 }
