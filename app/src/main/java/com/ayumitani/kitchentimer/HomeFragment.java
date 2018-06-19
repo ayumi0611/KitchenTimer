cba609b7a8c47fc157838e38b0566dc8733a03cd
ayumitani <ayumi.minamitani@gmail.com>
2018-06-19 22:12:57 +0900

    Revert "HomeFragmentの変更"
    This reverts commit 5b9f5ab3de985a8b39e5700cc07d2947c4d27db3.

--- app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
+++ app/src/main/java/com/ayumitani/kitchentimer/HomeFragment.java
@@ -2,19 +2,16 @@ package com.ayumitani.kitchentimer;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.NumberPicker;
+import android.widget.ImageButton;
+import android.widget.QuickContactBadge;
 import android.widget.TextView;
-
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
@@ -32,44 +29,34 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
-        NumberPicker numberPicker1 = view.findViewById(R.id.picker1);
-        NumberPicker numberPicker2 = view.findViewById(R.id.picker2);
-
-        numberPicker1.setMaxValue(59);
-        numberPicker1.setMinValue(0);
-
-        numberPicker2.setMaxValue(59);
-        numberPicker2.setMinValue(0);
-
-        final LinearLayout linearLayout = view.findViewById(R.id.time_picker);
-        final TextView textView = view.findViewById(R.id.timer);
-
-
         // viewをidから取得する
         FloatingActionButton transitionRecipeListButton = view.findViewById(R.id.transition_recipe_list);
         // viewにOnClickListenerをセットする
         transitionRecipeListButton.setOnClickListener(new OnClickTransitionRecipeListListener());
 
-        Button startButton = view.findViewById(R.id.button_start);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                linearLayout.setVisibility(View.GONE);
-                textView.setVisibility(View.VISIBLE);
+        // button_plusのviewをidから取得する
+        ImageButton button_plus_1 = view.findViewById(R.id.button_plus_1);
+        ImageButton button_plus_2 = view.findViewById(R.id.button_plus_2);
+        ImageButton button_plus_3 = view.findViewById(R.id.button_plus_3);
+        ImageButton button_plus_4 = view.findViewById(R.id.button_plus_4);
 
-            }
-        });
+        // button_plusのviewにOnClickListenerをセットする
+        button_plus_1.setOnClickListener(new OnClickButtonPlus1());
+        button_plus_2.setOnClickListener(new OnClickButtonPlus2());
+        button_plus_3.setOnClickListener(new OnClickButtonPlus3());
+        button_plus_4.setOnClickListener(new OnClickButtonPlus4());
 
-        Button resetButton = view.findViewById(R.id.button_reset);
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                linearLayout.setVisibility(View.VISIBLE);
-                textView.setVisibility(View.GONE);
-            }
-        });
+        // button_minusのviewをidから取得する
+        ImageButton button_minus_1 = view.findViewById(R.id.button_minus_1);
+        ImageButton button_minus_2 = view.findViewById(R.id.button_minus_2);
+        ImageButton button_minus_3 = view.findViewById(R.id.button_minus_3);
+        ImageButton button_minus_4 = view.findViewById(R.id.button_minus_4);
 
+        //　button_minusのviewにOnClickListenerをセットする
+        button_minus_1.setOnClickListener(new OnClickButtonMinus1());
+        button_minus_2.setOnClickListener(new OnClickButtonMinus2());
+        button_minus_3.setOnClickListener(new OnClickButtonMinus3());
+        button_minus_4.setOnClickListener(new OnClickButtonMinus4());
 
     }
 
@@ -84,41 +71,136 @@ public class HomeFragment extends Fragment {
         }
     }
 
+    // button_plusのOnClickListenerメソッド
+    private class OnClickButtonPlus1 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.ten_minutes);
+            String replaceTenMinutes = textView.getText().toString();
+            int tenMinutes = Integer.parseInt(replaceTenMinutes);
+            if (tenMinutes >= 9){
+                tenMinutes = 0;
+            } else {
+                tenMinutes++;
+            }
+            replaceTenMinutes = Integer.toString(tenMinutes);
+            textView.setText(replaceTenMinutes);
+            }
 
-    private void tick(){}
+        }
 
-    private void finishTimer(){
-        //v.playSoundEffect(SoundEffectConstants.CLICK);
-
+    private class OnClickButtonPlus2 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.one_minutes);
+            String replaceOneMinutes= textView.getText().toString();
+            int oneMinutes = Integer.parseInt(replaceOneMinutes);
+            if (oneMinutes >= 9){
+                oneMinutes = 0;
+            } else {
+                oneMinutes++;
+            }
+            replaceOneMinutes = Integer.toString(oneMinutes);
+            textView.setText(replaceOneMinutes);
+        }
 
     }
 
-    private class KitchenTimer extends CountDownTimer {
-
-        private SimpleDateFormat dateFormat = new SimpleDateFormat("mm:ss", Locale.JAPAN);
-
-        /**
-         * @param millisInFuture    The number of millis in the future from the call
-         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
-         *                          is called.
-         * @param countDownInterval The interval along the way to receive
-         *                          {@link #onTick(long)} callbacks.
-         */
-        public KitchenTimer(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
-
+    private class OnClickButtonPlus3 implements View.OnClickListener{
         @Override
-        public void onTick(long millisUntilFinished) {
-            tick();
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.ten_seconds);
+            String replaceTenSeconds = textView.getText().toString();
+            int tenSeconds = Integer.parseInt(replaceTenSeconds);
+            if (tenSeconds >= 5){
+                tenSeconds = 0;
+            } else {
+                tenSeconds++;
+            }
+            replaceTenSeconds = Integer.toString(tenSeconds);
+            textView.setText(replaceTenSeconds);
         }
 
+    }
+
+    private class OnClickButtonPlus4 implements View.OnClickListener{
         @Override
-        public void onFinish() {
-            // 完了
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.one_seconds);
+            String replaceOneSeconds = textView.getText().toString();
+            int oneSeconds = Integer.parseInt(replaceOneSeconds);
+            if (oneSeconds >= 9){
+                oneSeconds = 0;
+            } else {
+                oneSeconds++;
+            }
+            replaceOneSeconds = Integer.toString(oneSeconds);
+            textView.setText(replaceOneSeconds);
         }
+    }
 
+    // button_minusのOnClickListenerメソッド
+    private class OnClickButtonMinus1 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.ten_minutes);
+            String replaceTenMinutes = textView.getText().toString();
+            int tenMinutes = Integer.parseInt(replaceTenMinutes);
+            if (tenMinutes <= 0){
+                tenMinutes = 9;
+            } else {
+                tenMinutes--;
+            }
+            replaceTenMinutes = Integer.toString(tenMinutes);
+            textView.setText(replaceTenMinutes);
+        }
+    }
 
+    private class OnClickButtonMinus2 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.one_minutes);
+            String replaceOneMinutes = textView.getText().toString();
+            int oneMinutes = Integer.parseInt(replaceOneMinutes);
+            if (oneMinutes <= 0){
+                oneMinutes = 9;
+            } else {
+                oneMinutes--;
+            }
+            replaceOneMinutes = Integer.toString(oneMinutes);
+            textView.setText(replaceOneMinutes);
+        }
+    }
+
+    private class OnClickButtonMinus3 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.ten_seconds);
+            String replaceTenSeconds = textView.getText().toString();
+            int tenSeconds = Integer.parseInt(replaceTenSeconds);
+            if (tenSeconds <= 0){
+                tenSeconds = 5;
+            } else {
+                tenSeconds--;
+            }
+            replaceTenSeconds = Integer.toString(tenSeconds);
+            textView.setText(replaceTenSeconds);
+        }
+    }
+
+    private class OnClickButtonMinus4 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            TextView textView = getView().findViewById(R.id.one_seconds);
+            String replaceOneSeconds = textView.getText().toString();
+            int oneSeconds = Integer.parseInt(replaceOneSeconds);
+            if (oneSeconds <= 0){
+                oneSeconds = 9;
+            } else {
+                oneSeconds--;
+            }
+            replaceOneSeconds = Integer.toString(oneSeconds);
+            textView.setText(replaceOneSeconds);
+        }
     }
 }
-
